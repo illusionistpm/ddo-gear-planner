f2984c318730b91942af675c1d337268255a6776
jonathan-koppenhaver <34815887+jonathan-koppenhaver@users.noreply.github.com>
2019-12-20 10:51:06 -0500

    typeahead component

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GearListComponent } from './gear-list/gear-list.component';
 import { GearComponent } from './gear/gear.component';
+import { TypeaheadComponent } from './typeahead/typeahead.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GearListComponent,
-    GearComponent
+    GearComponent,
+    TypeaheadComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

385aef38f8950ffadbcb567551cee6a19607b9e4
jonathan-koppenhaver <34815887+jonathan-koppenhaver@users.noreply.github.com>
2020-01-22 23:57:31 -0500

    cloud on its own page

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { AffixCloudComponent } from './affix-cloud/affix-cloud.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent }
+  { path: '', component: AffixCloudComponent },
+  { path: 'main', component: MainComponent }
 ];
 
 @NgModule({

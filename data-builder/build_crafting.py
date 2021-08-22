b9279432dea06bc6cbf00fdc06dc20aa99d7f64f
Jonathan Koppenhaver <34815887+illusionistpm@users.noreply.github.com>
2021-08-22 17:34:44 -0400

    fix

--- data-builder/build_crafting.py
+++ data-builder/build_crafting.py
@@ -2,9 +2,10 @@ import json
 from parse_slavers import parse_slavers_crafting
 from parse_augments import parse_augments
 from write_json import write_json
+from read_json import read_json
 
 def build_crafting():
-    nearlyFinished = json.load(open(f"{os.path.dirname(__file__)}/nearly-finished.json", "r", encoding='utf-8'))
+    nearlyFinished = read_json('nearly-finished')
 
     slavers = parse_slavers_crafting()
 

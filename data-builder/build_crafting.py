e30e87273eff014c0cfe558d24029640e2e14ca7
joenuts <>
2024-09-03 05:18:32 +0000

    Change item augment data retrieval from item augment category page to use dedicated raw data page Update processing of items cached files to parse raw data item augment page first Update code that writes crafting json to sort entries alphabetically to help with identifying drift moving forward Add support to process isle of dread custom affixes on artifacts and quarterstaffs while parsing items Add various synonyms to support newer item affix inconsistencies Remove code that processed dinosaur bone crafting items and augments (now processed by items code)

--- data-builder/build_crafting.py
+++ data-builder/build_crafting.py
@@ -3,7 +3,6 @@ from get_inverted_synonym_map import get_inverted_synonym_map
 from parse_slavers import parse_slavers_crafting
 from parse_augments import parse_augments
 from write_json import write_json
-from parse_dinosaur_bone_crafting import parse_dinosaur_bone_crafting
 from get_lost_purpose import get_lost_purpose_crafting
 import os
 
@@ -13,8 +12,6 @@ def build_crafting():
 
     slavers = parse_slavers_crafting()
 
-    dino_bone = parse_dinosaur_bone_crafting()
-
     lost_purpose = get_lost_purpose_crafting()
 
     augments = parse_augments()
@@ -22,7 +19,6 @@ def build_crafting():
     combined = {}
     combined.update(nearlyFinished)
     combined.update(slavers)
-    combined.update(dino_bone)
     combined.update(augments)
     combined.update(lost_purpose)
 
